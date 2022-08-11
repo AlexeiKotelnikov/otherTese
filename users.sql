67da342ed2ff507e0566196c2059ba177eca29f5
kotelnikov.a_qschool.newgrade <ko7elnikoval@yandex.ru>
2022-08-11 14:40:01 +0300

    Second commit - doc block writing

--- users.sql
+++ users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `users` (
                          `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
                          `name` varchar(128) NOT NULL,
-                         `second_name` varchar(128) NOT NULL,
+                         `last_name` varchar(128) NOT NULL,
                          `dt_birth` date NOT NULL,
                          `gender` tinyint(4) NOT NULL DEFAULT 0,
                          `city` varchar(128) NOT NULL
